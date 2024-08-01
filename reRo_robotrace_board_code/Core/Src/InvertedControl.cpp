40a25d3743d6e46628d00eb13f0237801100d172
haruki shimotori <underbird.h@gmail.com>
2024-08-01 21:09:21 +0900

    エンコーダ角速度とかOK

--- reRo_robotrace_board_code/Core/Src/InvertedControl.cpp
+++ reRo_robotrace_board_code/Core/Src/InvertedControl.cpp
@@ -17,7 +17,7 @@ double mon_estimated_robot_theta;
 InvertedControl::InvertedControl(DriveMotor *motor, Encoder *encoder, IMU *imu): kp_(0), ki_(0), kd_(0), i_reset_flag_(0),
 		pre_P_{0.1*M_PI/180, 0, 0, 6.3e-06}, pre_theta_(0), U_(6.3e-06), W_(2.2e-05), estimated_robot_theta_(0), //U: 角速度の分散, W: 角度の分散
 		pre_xb_{0, 0, 0, 0}, xb_{0, 0, 0, 0}, dt_(1e-3), input_(0), target_theta_(0), z_(0), current_voltage_(8.4), target_omega_(0),
-		pre_target_theta_(0), pre_z_(0), pre_input_(0), disturbance_{0, 0, 0, 0}, f_{-2.6052e+03, -388.0362, -55.6586, -71.1086}, k_(-100)
+		pre_target_theta_(0), pre_z_(0), pre_input_(0), disturbance_{0, 0, 0, 0}, f_{-2.6052e3, -388.0362, -55.6586, -71.1086}, k_(-100)
 {
 	motor_ = motor;
 	encoder_ = encoder;
