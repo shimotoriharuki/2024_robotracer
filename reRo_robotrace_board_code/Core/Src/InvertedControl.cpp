69c9473200372393d89220e738c0f3d70d3c6a0a
haruki shimotori <underbird.h@gmail.com>
2024-08-07 21:05:17 +0900

    ふらｒふらするけど倒立する

--- reRo_robotrace_board_code/Core/Src/InvertedControl.cpp
+++ reRo_robotrace_board_code/Core/Src/InvertedControl.cpp
@@ -23,7 +23,7 @@ double mon_z;
 InvertedControl::InvertedControl(DriveMotor *motor, Encoder *encoder, IMU *imu): kp_(0), ki_(0), kd_(0), i_reset_flag_(0),
 		pre_P_{0.1*M_PI/180, 0, 0, 6.3e-06}, pre_theta_(0), U_(6.3e-06), W_(2.2e-05), estimated_robot_theta_(0), //U: 角速度の分散, W: 角度の分散
 		pre_xb_{0, 0, 0, 0}, xb_{0, 0, 0, 0}, dt_(1e-3), input_(0), target_theta_(0), z_(0), current_voltage_(8.4), target_omega_(0),
-		pre_target_theta_(0), pre_z_(0), pre_input_(0), disturbance_{0, 0, 0, 0}, f_{-25.8756229446122,-2.49212528236434,-0.0773490334065588,-0.154512658703037}, k_(-0.0408)
+		pre_target_theta_(0), pre_z_(0), pre_input_(0), disturbance_{0, 0, 0, 0}, f_{-30.3368634963338,-3.18596302460829,-0.180337636698537,-0.282974069083134}, k_(-0.0707)
 {
 	motor_ = motor;
 	encoder_ = encoder;
