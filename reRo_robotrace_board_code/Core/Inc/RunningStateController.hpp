898544a4c806f8b5f7e9da20e0bc9445c319a1f1
haruki shimotori <underbird.h@gmail.com>
2024-10-27 14:49:48 +0900

    二次走行うごいてそう

--- reRo_robotrace_board_code/Core/Inc/RunningStateController.hpp
+++ reRo_robotrace_board_code/Core/Inc/RunningStateController.hpp
@@ -20,7 +20,7 @@
 #include "Logger.hpp"
 #include "InvertedControl.hpp"
 
-#define COURSE_STORAGE_SIZE 5000
+#define COURSE_STORAGE_SIZE 3500
 #define GOAL_LINE_JUDGE_DISTANCE 100 //mm //70
 #define SIDE_LINE_JUDGE_DISTANCE 70 //mm
 
