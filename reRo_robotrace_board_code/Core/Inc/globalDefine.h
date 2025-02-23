4c66e90eb58e8db3d85ad1a03696ed4a0f1c71de
haruki shimotori <underbird.h@gmail.com>
2025-02-23 09:58:22 +0900

    止まる時間短くとゲイン下げた

--- reRo_robotrace_board_code/Core/Inc/globalDefine.h
+++ reRo_robotrace_board_code/Core/Inc/globalDefine.h
@@ -34,9 +34,9 @@
 #define F4_RUN -0.0871
 
 #define F1_STOP -20.0
-#define F2_STOP -2.5
+#define F2_STOP -2.0
 #define F3_STOP -0.10
-#define F4_STOP -0.25
+#define F4_STOP -0.20
 //#define F1_STOP -30.0
 //#define F2_STOP -3.5
 //#define F3_STOP -0.10
